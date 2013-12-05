d052a00b964519565c247f771001a44f152173a2
Steffan Chartrand <schartrand@twitter.com>
2013-12-04 23:23:55 -0800

    another way to calculate and documentation for triplets

--- Python/Calculator.py
+++ Python/Calculator.py
@@ -1,26 +1,46 @@
-def operation(s):
+# edge cases, testing
+# handling whitespace
+# order of operations
+
+# division and multiplication left to right, addition and subtraction left to right
+# ex: 1/2*4, need to do division first
+# ex: 1-1+2, need to do subtraction first
+
+# valid equation (only symbols or ints or floats, start and end are both ints or floats)
+# division by 0
+
+# questions
+# why would you do this linearly vs recursively?
+# what would you expect to be faster?
+
+import re
+import timeit
+
+def do_math(s):
     a = s.split('+',1)
     if len(a) == 2:
-        return operation(a[0]) + operation(a[1])
+        return do_math(a[0]) + do_math(a[1])
 
     a = s.split('-',1)
     if len(a) == 2:
-        return operation(a[0]) - operation(a[1])
+        return do_math(a[0]) - do_math(a[1])
 
     a = s.split('*',1)
     if len(a) == 2:
-        return operation(a[0]) * operation(a[1])
+        return do_math(a[0]) * do_math(a[1])
 
     a = s.split('/',1)
     if len(a) == 2:
-        return operation(a[0]) / operation(a[1])
+        return do_math(a[0]) / do_math(a[1])
 
     return float(s)
 
-print operation('1+1-1/2*6')
-print operation('1')
+print "DERP1"
+print do_math('1+1-1/2*6')
+print do_math('1+1-2+1')
+print do_math('1')
 
-def do_math(s):
+def do_math1(s):
     return add(s)
 
 def add(s):
@@ -51,5 +71,52 @@ def divide(s):
         out/=float(i)
     return out
 
-print do_math('1+1-1/3')
-print do_math('1')
+print "DERP2"
+print do_math1('1 +1-1/3')
+print do_math('1+1-2+1')
+print do_math1('1')
+
+
+def update_equation(equation, i, res):
+    equation[i-1] = ''
+    equation[i] = ''
+    equation[i+1] = res
+
+def do_math2(s):
+    s.replace(' ', '')
+    equation = re.split(r'(\D)', s)
+
+    for i,n in enumerate(equation):
+        if n == '/':
+            res = int(equation[i-1]) / float(equation[i+1])
+            update_equation(equation, i, res)
+
+        if n == '*':
+            res = float(equation[i-1]) * float(equation[i+1])
+            update_equation(equation, i, res)
+
+    equation = filter(lambda x: x != '', equation)
+
+    for i,n in enumerate(equation):
+        if n == '-':
+            res = int(equation[i-1]) - float(equation[i+1])
+            update_equation(equation, i, res)
+
+        if n == '+':
+            res = float(equation[i-1]) + float(equation[i+1])
+            update_equation(equation, i, res)
+
+    equation = filter(lambda x: x != '', equation)
+
+    return equation[0]
+
+print "DERP3"
+print do_math2('2+24*48/32')
+
+
+print ""
+print "TIMINGS"
+number=1000
+print timeit.timeit("do_math('1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1')", setup='from __main__ import do_math', number=number)
+print timeit.timeit("do_math1('1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1')", setup='from __main__ import do_math1', number=number)
+print timeit.timeit("do_math2('1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1')", setup='from __main__ import do_math2', number=number)
--- Python/Triplets.py
+++ Python/Triplets.py
@@ -1,3 +1,9 @@
+# given a list of triplets [(string1, string2, int), (string1, string2, int)...]
+# write a function that prints the sum and average of ints where string1 and
+# string2 of one triplet equal string1 and string2 of another triplet
+# for example: ('ab', 'cd', 2) and ('ab','cd', 4) and any other triplets with
+# matching string1 and string2 would be summed and averaged
+
 a = [('a','b',1), ('a','b',2), ('c','d',3), ('ab','c',2), ('a','bc',2)]
 
 def compute(l):
@@ -27,3 +33,8 @@ def unit_test():
     a = [('a','b',1), ('a','b',2), ('c','d',3)]
 
 unit_test()
+
+firstString -> list of triplets per first string
+secondString -> list of triplets per second string
+what if first strings are different but have matching seond strings?
+looks like they're going to be in the same bucket for the second string hash table
